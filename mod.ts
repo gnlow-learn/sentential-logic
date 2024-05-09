f018cdca82310f677bba9412c5b2e9af30d385a6
Gnlow <gnlow@protonmail.com>
2024-05-09 11:33:42 +0900

    wtf

--- mod.ts
+++ mod.ts
@@ -9,3 +9,37 @@ const { and, or, if_, iff }: Record<string, Binary> = {
     if_: (p, q) => !p || q,
     iff: (p, q) => p == q,
 }
+
+const char = {
+    AND: "&",
+    OR:  "v",
+    IF:  "→",
+    IFF: "↔",
+    NOT: "~",
+} as const
+
+const tokenize =
+(str: string) =>
+    str
+        .replaceAll("<->", char.IFF)
+        .replaceAll("->",  char.IF)
+        .replaceAll("|",   char.OR)
+        .replaceAll("&",   char.AND)
+        .replaceAll("~",   char.NOT)
+
+const lex =
+(tokens: string) =>
+    tokens == "" ? []
+    : /\(/.test(tokens) ? [
+        ...lex(/^(.*?)\(/.exec(tokens)?.[1] || ""),
+        ...lex(/\((.*)\)/.exec(tokens)?.[1] || ""),
+        ...lex(/\)([^)]*?)$/.exec(tokens)?.[1] || "",)
+    ]
+    : tokens[0] == char.NOT ? ["NOT", tokens.substring(1)]
+    : [
+        Object.entries(char).find(([k, v]) => v == tokens[1]),
+        tokens[0],
+        tokens[2],
+    ]
+
+console.log(lex(tokenize("(p<->q)->r")))
